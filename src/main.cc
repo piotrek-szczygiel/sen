b0ba458d9d60d69f008ec4af8dddec33daecdf69
Piotr Szczygieł <szczygiel.p@gmail.com>
2020-04-24 01:29:40 +0200

    Make ps.hh header only

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,11 @@
+#ifndef NDEBUG
+#define PS_HH_LEAK_CHECK
+#endif
+
+#define PS_HH_IMPLEMENTATION
 #include "ps.hh"
 
 int main() {
-    defer(mem_check_dump());
+    defer(mem_leak_check());
     return 0;
 }
--- src/ps.hh
+++ src/ps.hh
@@ -1,10 +1,6 @@
 #pragma once
 // Piotr Szczygieł library thing
 
-#ifndef NDEBUG
-#define PS_MEM_DEBUG  // Enable memory leak tracing on debug builds
-#endif
-
 #include <assert.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -19,7 +15,7 @@ using i64 = int64_t;
 using u8 = uint8_t;
 
 // Heap definitions
-void mem_check_dump();
+void mem_leak_check();
 
 #define mem_alloc(count) _mem_alloc((count), __FILE__, __LINE__)
 #define mem_free(ptr) _mem_free(ptr, __FILE__, __LINE__)
@@ -98,14 +94,35 @@ void list_free(List<T>* list) {
 }
 
 // Hashmap
-usize hm_fn_hash(const char* key);
-bool hm_fn_eq(const char* k1, const char* k2);
-
-usize hm_fn_hash(usize key);
-bool hm_fn_eq(usize k1, usize k2);
-
 #define HM_ROTATE_LEFT(val, n) (((val) << (n)) | ((val) >> (sizeof(usize) * 8 - (n))))
 #define HM_ROTATE_RIGHT(val, n) (((val) >> (n)) | ((val) << (sizeof(usize) * 8 - (n))))
+
+inline usize hm_fn_hash(const char* key) {
+    assert(key);
+    constexpr usize seed = 0x1234567890ABCDEF;
+    usize hash = seed;
+    while (*key) hash = HM_ROTATE_LEFT(hash, 9) + (u8)*key++;
+    hash ^= seed;
+    hash = (~hash) + (hash << 18);
+    hash ^= hash ^ HM_ROTATE_RIGHT(hash, 31);
+    hash = hash * 21;
+    hash ^= hash ^ HM_ROTATE_RIGHT(hash, 11);
+    hash += (hash << 6);
+    hash ^= HM_ROTATE_RIGHT(hash, 22);
+    return (hash + seed);
+}
+
+inline usize hm_fn_hash(usize key) {
+    return key;
+}
+
+inline bool hm_fn_eq(const char* k1, const char* k2) {
+    return strcmp(k1, k2) == 0;
+}
+
+inline bool hm_fn_eq(usize k1, usize k2) {
+    return k1 == k2;
+}
 
 template <typename K, typename V>
 struct Hash_Entry {
@@ -195,3 +212,181 @@ void hm_free(Hash_Map<K, V>* hm) {
     mem_free(hm->table);
     mem_free(hm);
 }
+
+// Functions that can't be in shared header file
+#ifdef PS_HH_IMPLEMENTATION
+
+// Debug allocator implementation
+#ifdef PS_HH_LEAK_CHECK
+struct _mem_entry {
+    usize ptr;
+    usize size;
+    bool freed;
+    bool reallocated;
+    const char* file;
+    int line;
+    _mem_entry* next;
+};
+
+constexpr usize _mem_entry_size = 16;
+static _mem_entry* _mem_allocations[_mem_entry_size] = {0};
+
+void mem_leak_check() {
+    usize leaks = 0;
+    for (usize i = 0; i < _mem_entry_size; i++) {
+        auto entry = _mem_allocations[i];
+        while (entry) {
+            if (!entry->freed) {
+                printf("(%s:%d) Leaked %llu bytes. (%p)\n", entry->file, entry->line, entry->size, (void*)entry->ptr);
+                ++leaks;
+            }
+
+            auto tmp = entry;
+            entry = entry->next;
+            free(tmp);
+        }
+    }
+    if (!leaks) printf("No memory leaks detected.\n");
+}
+#else
+void mem_leak_check() {}  // Memory leak checking is disabled on release builds
+#endif
+
+void* _mem_alloc(usize size, const char* file, int line) {
+    auto ptr = calloc(size, 1);
+    assert(ptr);
+
+#ifdef PS_HH_LEAK_CHECK
+    auto entry = _mem_allocations[(usize)ptr % _mem_entry_size];
+    bool found = false;
+    _mem_entry* prev = nullptr;
+    while (entry) {
+        prev = entry;
+        if (entry->ptr == (usize)ptr) {
+            found = true;
+            if (!entry->freed) fprintf(stderr, "%s:%d: memory allocated at %p even though it wasn't free\n", file, line, ptr);
+            entry->size = size;
+            entry->freed = false;
+            entry->file = file;
+            entry->line = line;
+            break;
+        }
+        entry = entry->next;
+    }
+    if (!found) {
+        auto new_entry = (_mem_entry*)calloc(1, sizeof(_mem_entry));
+        new_entry->ptr = (usize)ptr;
+        new_entry->size = size;
+        new_entry->freed = false;
+        new_entry->file = file;
+        new_entry->line = line;
+        if (prev) {
+            new_entry->next = prev->next;
+            prev->next = new_entry;
+        } else {
+            _mem_allocations[(usize)ptr % _mem_entry_size] = new_entry;
+        }
+    }
+#else
+    (void)file;
+    (void)line;
+#endif
+
+    return ptr;
+}
+
+void _mem_free(void* ptr, const char* file, int line) {
+    assert(ptr);
+
+#ifdef PS_HH_LEAK_CHECK
+    bool found = false;
+    auto entry = _mem_allocations[(usize)ptr % _mem_entry_size];
+    while (entry) {
+        if (entry->ptr == (usize)ptr) {
+            found = true;
+            if (entry->freed) fprintf(stderr, "%s:%d: freeing already freed memory at %p\n", file, line, ptr);
+            entry->freed = true;
+            break;
+        }
+        entry = entry->next;
+    }
+    if (!found) fprintf(stderr, "%s:%d: freeing memory before using at %p\n", file, line, ptr);
+#else
+    (void)file;
+    (void)line;
+#endif
+
+    free(ptr);
+}
+
+void* _mem_realloc(void* old_ptr, usize old_size, usize new_size, const char* file, int line) {
+    if (!old_ptr) return _mem_alloc(new_size, file, line);
+
+    auto new_ptr = realloc(old_ptr, new_size);
+    assert(new_ptr);
+    memset((u8*)new_ptr + old_size, 0, new_size - old_size);
+
+#ifdef PS_HH_LEAK_CHECK
+    if (old_ptr == new_ptr) {  // We had enough memory in our current block
+        auto ptr = old_ptr;
+        bool found = false;
+        auto entry = _mem_allocations[(usize)ptr % _mem_entry_size];
+        while (entry) {
+            if (entry->ptr == (usize)ptr) {
+                found = true;
+                entry->size = new_size;
+                if (entry->freed) fprintf(stderr, "%s:%d: freed pointer passed to realloc at %p\n", file, line, ptr);
+                entry->freed = false;
+                break;
+            }
+            entry = entry->next;
+        }
+        if (!found) fprintf(stderr, "%s:%d: couldn't find previous allocation while reallocating at %p\n", file, line, ptr);
+    } else {  // We got new memory on the heap
+        bool found = false;
+        auto entry = _mem_allocations[(usize)old_ptr % _mem_entry_size];
+        while (entry) {  // Let's remove previous address
+            if (entry->ptr == (usize)old_ptr) {
+                found = true;
+                entry->freed = true;
+                entry->reallocated = true;
+                break;
+            }
+            entry = entry->next;
+        }
+
+        if (!found) {
+            fprintf(stderr, "%s:%d: old pointer not found while reallocating %p -> %p\n", file, line, old_ptr, new_ptr);
+        }
+
+        entry = _mem_allocations[(usize)new_ptr % _mem_entry_size];
+        auto prev = entry;
+        while (entry) {
+            prev = entry;
+            if (entry->ptr == (usize)new_ptr) {
+                if (!entry->freed) fprintf(stderr, "%s:%d: memory allocated at %p even though it wasn't free\n", file, line, new_ptr);
+                break;
+            }
+            entry = entry->next;
+        }
+
+        entry = (_mem_entry*)calloc(1, sizeof(_mem_entry));
+        entry->ptr = (usize)new_ptr;
+        entry->size = new_size;
+        entry->freed = false;
+        entry->file = file;
+        entry->line = line;
+        entry->next = nullptr;
+        if (prev) {
+            entry->next = prev->next;
+            prev->next = entry;
+        } else {
+            _mem_allocations[(usize)new_ptr % _mem_entry_size] = entry;
+        }
+    }
+#endif
+
+    return new_ptr;
+}
+
+#endif  // PS_HH_IMPLEMENTATION
